3b1920cca13cbd3bec78ec676ee0efa94b6937c9
amychavez1311 <amychavez131125@gmail.com>
2026-02-03 11:56:19 -0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -52,10 +52,11 @@ TEMPLATES = [
 ]
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 LANGUAGE_CODE = 'es-ec'
